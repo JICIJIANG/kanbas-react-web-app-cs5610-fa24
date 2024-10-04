a106907204965a13a88b74fbef4d716fe9d6d3fb
JiciJiang <jicijiang@Benjamins-MacBook-Pro.local>
2024-10-03 23:24:02 -0400

    a2 CSS and Bootstrap

--- src/Kanbas/Courses/Home/index.js
+++ src/Kanbas/Courses/Home/index.js
@@ -4,11 +4,11 @@ import CourseStatus from "./Status";
   export default function Home() {
     return (
       <div className="row" id="wd-home">
-        <div className="col-md-8s col-sm-11">
+        <div className="col-md-9 col-sm-12">
           <Modules />
         </div>
   
-        <div className="col-md-2 d-none d-md-block">
+        <div className="col-md-3 d-none d-md-block">
           <CourseStatus />
         </div>
       </div>
